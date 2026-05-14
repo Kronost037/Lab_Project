09cc39dd0594e956a1eaaf886c4e3764d1fe7e80
Raad <kronost037@gmail.com>
2026-05-14 21:42:50 +0600

    Indent game.c

--- game.c
+++ game.c
@@ -1,78 +1,80 @@
 #define SDL_MAIN_USE_CALLBACKS 1
- 
+
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
- 
+
 #define CHECK(x) do { if(!x) goto ERROR; } while(0)
- 
+
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
- 
+
 typedef SDL_AppResult GameStatus;
- 
+
 typedef struct {
-SDL_Window *window;
-SDL_Renderer *renderer;
+    SDL_Window *window;
+    SDL_Renderer *renderer;
 } MyGame;
- 
+
 GameStatus SDL_AppInit(void **appstate, int argc, char **argv) {
-(void) argc;
-(void) argv;
- 
-MyGame *game = (MyGame *)SDL_calloc(1, sizeof(MyGame));
-CHECK(game);
- 
-bool display = SDL_CreateWindowAndRenderer("Hello World", SCREEN_WIDTH, SCREEN_HEIGHT, 0, &game->window, &game->renderer);
-CHECK(display);
- 
-*appstate = game;
-return SDL_APP_CONTINUE;
- 
-ERROR:
-if(game) {
-SDL_free(game);
-SDL_Log("ERROR: APPInit FAILED: %s", SDL_GetError());
+    (void) argc;
+    (void) argv;
+    
+    MyGame *game = (MyGame *)SDL_calloc(1, sizeof(MyGame));
+    CHECK(game);
+    
+    bool display = SDL_CreateWindowAndRenderer("Hello World", SCREEN_WIDTH, SCREEN_HEIGHT, 0, &game->window, &game->renderer);
+    CHECK(display);
+    
+    *appstate = game;
+    return SDL_APP_CONTINUE;
+    
+ ERROR:
+    if(game) {
+        SDL_free(game);
+        SDL_Log("ERROR: APPInit FAILED: %s", SDL_GetError());
+    }
+    return SDL_APP_FAILURE;
 }
-return SDL_APP_FAILURE;
-}
- 
+
 GameStatus SDL_AppEvent(void *appstate, SDL_Event *event) {
-MyGame *game = (MyGame *)appstate;
-(void)game;
- 
-switch(event->type) {
-case SDL_EVENT_QUIT: return SDL_APP_SUCCESS;
-case SDL_EVENT_KEY_DOWN: {
-switch(event->key.key) {
-case SDLK_ESCAPE: return SDL_APP_SUCCESS;
+    MyGame *game = (MyGame *)appstate;
+    (void)game;
+    
+    switch(event->type) {
+    case SDL_EVENT_QUIT:
+        return SDL_APP_SUCCESS;
+    case SDL_EVENT_KEY_DOWN: {
+        switch(event->key.key) {
+        case SDLK_ESCAPE:
+            return SDL_APP_SUCCESS;
+        }
+    }
+    }
+    
+    return SDL_APP_CONTINUE;
 }
-}
-}
- 
-return SDL_APP_CONTINUE;
-}
- 
+
 GameStatus SDL_AppIterate(void *appstate) {
-MyGame *game = (MyGame *) appstate;
- 
-SDL_SetRenderDrawColor(game->renderer, 250, 250, 250, 250);
-SDL_RenderClear(game->renderer);
-SDL_RenderPresent(game->renderer);
- 
-return SDL_APP_CONTINUE;
+    MyGame *game = (MyGame *) appstate;
+    
+    SDL_SetRenderDrawColor(game->renderer, 250, 250, 250, 250);
+    SDL_RenderClear(game->renderer);
+    SDL_RenderPresent(game->renderer);
+    
+    return SDL_APP_CONTINUE;
 }
- 
+
 void SDL_AppQuit(void *appstate, SDL_AppResult result) {
-if(appstate) {
-MyGame *game = (MyGame *) appstate;
- 
-SDL_DestroyRenderer(game->renderer);
-SDL_DestroyWindow(game->window);
- 
-SDL_free(game);
+    if(appstate) {
+        MyGame *game = (MyGame *) appstate;
+        
+        SDL_DestroyRenderer(game->renderer);
+        SDL_DestroyWindow(game->window);
+        
+        SDL_free(game);
+    }
+    
+    (void) result;
+    SDL_Quit();
 }
- 
-(void) result;
-SDL_Quit();
-}
- 
+
